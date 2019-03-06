b60235ce1fef94863e1fc38472a41253a6a3766a
iro <iro@sitecore.net>
2019-03-06 21:43:34 +0200

    check if devices element from layout XML is empty

--- src/Sitecore.Support.315324/InjectCompositeComponents.cs
+++ src/Sitecore.Support.315324/InjectCompositeComponents.cs
@@ -11,12 +11,16 @@
     public override IList<XElement> GetDevices(XElement layoutXml, ID contextDeviceId)
     {
       List<XElement> devices = layoutXml.Descendants("d").ToList();
-      IList<XElement> list = FilterDevicesByDeviceId(devices, contextDeviceId);
-      if (!list.Any() && Context.Device.FallbackDevice != null)
+      if (devices.Any())
       {
-        return GetDevices(layoutXml, Context.Device.FallbackDevice.ID);
+        IList<XElement> list = FilterDevicesByDeviceId(devices, contextDeviceId);
+        if (!list.Any() && Context.Device.FallbackDevice != null)
+        {
+          return GetDevices(layoutXml, Context.Device.FallbackDevice.ID);
+        }
+        return list;
       }
-      return list;
+      return new List<XElement>();
     }
   }
 }
